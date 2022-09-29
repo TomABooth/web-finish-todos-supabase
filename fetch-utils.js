6d3c9e6467134ae27740bcd02a656aa5c1081baf
Tom Booth <tombeezle@gmail.com>
2022-09-29 09:52:46 -0700

    added supabase api

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = 'https://nwxkvnsiwauieanvbiri.supabase.co';
+const SUPABASE_URL = 'https://qzjxjzoitkpoyrvazfle.supabase.co';
 const SUPABASE_KEY =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNzAwMzQzNCwiZXhwIjoxOTUyNTc5NDM0fQ.8XIsU0FANdaNeQnT-DojpTL-GTlTPZ4CYZDEetpFpWc';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF6anhqem9pdGtwb3lydmF6ZmxlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQ0Njg2MTMsImV4cCI6MTk4MDA0NDYxM30.-YXcZyjlSS51RFUvLqaaH6lUZ31UitcwsGaDmxSze_E';
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 /* Auth related functions */
